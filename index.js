535bbe0130a37e78350c906729ed649bdab886f2
Igor Rakush <irakush@aol.com>
2023-11-14 12:03:12 -0500

    add .slice()

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (arr) => [arr[0], arr[1]];
+const returnFirstTwoDrivers = (arr) => arr.slice(0, 2);
 
-const returnLastTwoDrivers = (arr) => [arr[arr.length-2], arr[arr.length-1]]
+const returnLastTwoDrivers = (arr) => arr.slice(-2);
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
